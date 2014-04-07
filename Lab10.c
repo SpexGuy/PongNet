b189db5ec09c965f669d34cf32b8eba7d1bb363c
Martin Wickham <spexguy070@gmail.com>
2014-04-07 13:29:56 -0500

    added comments, final cleanup
    Remember to set DEST_ADDR and OWN_ADDR

--- Lab10.c
+++ Lab10.c
@@ -4,10 +4,10 @@
 //*****************************************************************************
 // TITLE:			Networked Pong
 // 
-// NAME:		    Martin Wickham,
-//							Scott Hendrickson,
-//							Kody Greenbaum
-// LAB SECTION:		<301, 302, 303, or 304>
+// NAME:		    Martin Wickham (303),
+//							Scott Hendrickson (304),
+//							Kody Greenbaum (304)
+//
 // LAB DAY:			Monday, Wednesday
 //
 // CLASS:			ECE 210
@@ -44,6 +44,7 @@ void 	Xbee_Send(int message);
 void	Xbee_SendArray(char *s, int messageLength);
 
 
+/* sprite data definitions */
 #define ballWidth 6
 #define ballHeight 6
 static unsigned char ball_raster[28];
@@ -159,6 +160,7 @@ static unsigned char multiball_data[] =
 	0xAF, 0xA0, 0xAF, 0xA0};
 
 
+/* constant definitions */
 
 #define TRUE 1
 #define FALSE 0
@@ -192,21 +194,20 @@ static unsigned char multiball_data[] =
 #define SHORT_PADDLE 3
 #define NUM_POWERS 4
 
-#define DEST_ADDR 8
-#define OWN_ADDR 9
+#define DEST_ADDR 9
+#define OWN_ADDR 8
 #define SEND_ARRAY_LEN 6
 
 #define FRAME_WIDTH 64
 #define FRAME_HEIGHT 96
 #define FRAME_SIZE 6144
 
-//#define SCOREBOARD
-
-
+/* a boolean type */
 typedef unsigned short int bool;
 
 struct player;
 
+/* A Sprite can be drawn on screen */
 typedef struct sprite {
 	int width;
 	int height;
@@ -216,6 +217,7 @@ typedef struct sprite {
 	int yOffset;
 } Sprite;
 
+/* Balls move around the field */
 typedef struct ball {
 	bool active;
 	float x;
@@ -227,6 +229,7 @@ typedef struct ball {
 	Sprite sprite;
 } Ball;
 
+/* A Paddle deflects the ball */
 typedef struct paddle {
 	float x;
 	float y;
@@ -237,6 +240,7 @@ typedef struct paddle {
 	Sprite longSprite;
 } Paddle;
 
+/* A Player contains a Paddle and keeps track of powerups */
 typedef struct player {
 	bool ai;
 	int powerStatus[NUM_POWERS];
@@ -244,6 +248,7 @@ typedef struct player {
 	int points;
 } Player;
 
+/* A Powerup may be placed on the field */
 typedef struct power {
 	bool active;
 	bool available;
@@ -284,15 +289,23 @@ float mod(float a, float b);
 // Main Program:
 //
 //*****************************************************************************
+
+//the players
 Player players[2];
+//the balls
 Ball balls[NUM_BALLS];
+//the powerups
 Power powers[NUM_POWERS];
+//the loop count
 int time;
+//the statistics for the scoreboard
 char game_stats[6];
+//whether the scoreboard is in use
 bool scoreboard;
 
 int main(void) {
 	int c;
+
 	//Initializing the LEDBAR, RGB LED, DIPSwitches and Pushbuttons, and a wait timer
 	LEDBARInit();
 	DIPSWInit();
@@ -303,10 +316,12 @@ int main(void) {
 	potentiometersInit();
 	RIT128x96x4Clear();
 	
-	players[0].ai = read_Switches() & 0x0002;
+	//set up player 0
+	players[0].ai = read_Switches() & 0x0002; //check switch 4
 	players[0].points = 0;
 	players[0].paddle.y = PADDLE1_Y;
 
+	//set up player 0 sprites
 	players[0].paddle.midSprite.data = paddle_sprite_top;
 	players[0].paddle.midSprite.height = paddle_sprite_topHeight;
 	players[0].paddle.midSprite.width = paddle_sprite_topWidth;
@@ -328,11 +343,12 @@ int main(void) {
 	players[0].paddle.longSprite.yOffset = paddle_long_topHeight + ballHeight/2;
 	players[0].paddle.longSprite.raster = paddle_long_top_raster;
 	
-
-	players[1].ai = read_Switches() & 0x0001;
+	//set up player 1
+	players[1].ai = read_Switches() & 0x0001; //check switch 5
 	players[1].points = 0;
 	players[1].paddle.y = PADDLE2_Y;
 	
+	//set up player 1 sprites
 	players[1].paddle.midSprite.data = paddle_sprite_bottom;
 	players[1].paddle.midSprite.height = paddle_sprite_bottomHeight;
 	players[1].paddle.midSprite.width = paddle_sprite_bottomWidth;
@@ -355,18 +371,20 @@ int main(void) {
 	players[1].paddle.longSprite.raster = paddle_long_bottom_raster;
 
 
-
+	//set up player powers
 	for (c = 0; c < NUM_POWERS; c++) {
 		players[0].powerStatus[c] = 0;
 		players[1].powerStatus[c] = 0;
 	}
 	
+	//seed random based on switch 3
 	if (read_Switches() & 0x04) {
 		srand(2);
 	} else {
 		srand(readPotentiometer0() ^ readPotentiometer1() ^ readPotentiometer2());
 	}
 	
+	//set up powers
 	powers[MULTIBALL].active = FALSE;
 	powers[MULTIBALL].available = TRUE;
 	powers[MULTIBALL].prb = (int) (0.01 * RAND_MAX);
@@ -415,6 +433,7 @@ int main(void) {
 	powers[SHORT_PADDLE].sprite.xOffset = shortWidth/2;
 	powers[SHORT_PADDLE].sprite.raster = short_raster;
 	
+	//set up balls
 	for (c = 0; c < NUM_BALLS; c++) {
 		balls[c].active = FALSE;
 		balls[c].lastHit = players;
@@ -426,22 +445,26 @@ int main(void) {
 		balls[c].sprite.raster = ball_raster;
 	}
 	
+	//set up game
 	time = 0;
 	reset(TRUE);
 	
+	//check for scoreboard (switch 2)
 	scoreboard = read_Switches() & 0x08;
 	
 	if (scoreboard)
 		Xbee_ConfigureAddresses(DEST_ADDR, OWN_ADDR);
 	
-	RIT128x96x4StringDraw("pong", 0,8,15);
-	sysTickWait1mS(100);
-	RIT128x96x4Clear();
-	
+	//init scoreboard
 	send();
+	
+	//main loop
 	while(1) {
+		//update everything
 		update();
 		time++;
+		
+		//switch 1 alters wait time to poten 2
 		if (read_Switches() & 0x10)
 			sysTickWait1mS(readPotentiometer1());
 		else
@@ -449,6 +472,7 @@ int main(void) {
 	}
 }  	
 
+/* update everything */
 void update() {
 	updatePaddle();
 	updateBalls();
@@ -456,6 +480,7 @@ void update() {
 	updateScreen();
 }
 
+/* updates the ball positions */
 void updateBalls() {
 	float collX;
 	float trueSpeed;
@@ -466,17 +491,21 @@ void updateBalls() {
 	Ball *b;
 	int c;
 	
+	//for each ball
 	for(c = 0; c < NUM_BALLS; c++) {
+		//only update if ball is active
 		if (!balls[c].active)
 			continue;
 		
 		b = &balls[c];
 		
+		//update position
 		lastX = b->x;
 		lastY = b->y;
 		b->x += b->dx;
 		b->y += b->dy;
 		
+		//bounce off of sides
 		if (b->x >= FIELD_RIGHT) {
 			b->dx = -b->dx;
 			b->x = 2*FIELD_RIGHT - b->x - 1;
@@ -485,34 +514,48 @@ void updateBalls() {
 			b->x = 2*FIELD_LEFT - b->x;
 		}
 		
+		//bounce off of low paddle
 		if (b->y >= players[1].paddle.y) {
 			percent = (players[1].paddle.y - lastY) / (float) (b->y - lastY);
 			collX = mix(lastX, b->x, percent);
+			
+			//check for hit paddle
 			if (collX >= players[1].paddle.x && collX < players[1].paddle.x + paddleWidth(1)) {
+				//speed increases logarithmically
 				b->speed += .7;
 				trueSpeed = 3*log(b->speed + 1);
+				
+				//check for fastball
 				if (players[1].powerStatus[FASTBALL]) {
 					players[1].powerStatus[FASTBALL] = 0;
 					trueSpeed *= 2.5;
 					powers[FASTBALL].available = TRUE;
 					send();
 				}
+				
+				//calculate rebound angle
 				angle = mix(-ANGLE_MAX, ANGLE_MAX, (collX - players[1].paddle.x) / (float) paddleWidth(1));
+				
+				//calculate new dx and dy and position
 				b->dx = trueSpeed * sin(angle);
 				b->dy = -trueSpeed * cos(angle);
-				b->y = players[1].paddle.y + (1-percent)*b->dy; //TODO: fix this;
+				b->y = players[1].paddle.y + (1-percent)*b->dy;
 				b->x = collX + (1-percent)*b->dx;
 
+				//update last player to hit ball
 				b->lastHit = &players[1];
 
+				//update ai collision position
 				updateColl(0);
 				updateColl(1);
-
 			} else {
+				//paddle missed
 				b->active = FALSE;
 				scoreP1();
 			}
+		//check for hit upper paddle
 		} else if (b->y <= players[0].paddle.y) {
+			//check for paddle collision
 			percent = (players[0].paddle.y - lastY) / (float) (b->y - lastY);
 			collX = mix(lastX, b->x, percent);
 			if (collX >= players[0].paddle.x && collX < players[0].paddle.x + paddleWidth(0)) {
@@ -524,7 +567,9 @@ void updateBalls() {
 					powers[FASTBALL].available = TRUE;
 					send();
 				}
+				//calculate reflection angle
 				angle = mix(-ANGLE_MAX, ANGLE_MAX, (collX - players[0].paddle.x) / (float) paddleWidth(0));
+				//calculate new dx and dy
 				b->dx = trueSpeed * sin(angle);
 				b->dy = trueSpeed * cos(angle);
 				b->y = players[0].paddle.y + (1-percent)*b->dy; //TODO: fix this;
@@ -536,6 +581,7 @@ void updateBalls() {
 				updateColl(1);
 				
 			} else {
+				//p2 scored
 				b->active = FALSE;
 				scoreP2();
 			}
@@ -543,16 +589,19 @@ void updateBalls() {
 	}
 }
 
+/* count a score for player 1 */
 void scoreP1() {
 	players[0].points++;
 	send();
 	reset(TRUE);
 }
+/* count a score for player 2 */
 void scoreP2() {
 	players[1].points++;
 	send();
 	reset(FALSE);
 }
+/* reset the game board */
 void reset(bool p1Score) {
 	int c;
 	int sum = 0;
@@ -565,6 +614,7 @@ void reset(bool p1Score) {
 	if (sum != 0)
 		return;
 	
+	//launch a ball
 	balls[0].dx = START_DX;
 	balls[0].dy = p1Score ? -START_DY : START_DY;
 	balls[0].x = SCREEN_WIDTH/2;
@@ -579,12 +629,14 @@ void reset(bool p1Score) {
 		updateColl(1);
 }
 
+/* update the powerups */
 void updatePowers() {
 	int c, d;
 	float b1, b2, m1, m2, x, y;
 	Ball *b;
 	for (c = 0; c < NUM_POWERS; c++) {
 
+		//decrement if necessary
 		if (powers[c].decr) {
 			if (players[0].powerStatus[c] != 0) {
 				players[0].powerStatus[c]--;
@@ -609,17 +661,20 @@ void updatePowers() {
 				if (balls[d].active) {
 					
 					//collision detection
+					//find the closest point the ball came to the powerup
 					m1 = -balls[d].dy/balls[d].dx;
 					m2 = balls[d].dx/balls[d].dy;
 					b1 = balls[d].y - m1*balls[d].x;
 					b2 = powers[c].y - m2*powers[c].x;
 					x = (b2-b1)/(m1-m2);
 					y = m1*x + b1;
+					//if collision
 					if (((balls[d].y >= y && balls[d].y - balls[d].dy <= y) ||
 							 (balls[d].y <= y && balls[d].y - balls[d].dy >= y)) &&
 							abs(x - powers[c].x) <= powers[c].sprite.width &&
 							abs(y - powers[c].y) <= powers[c].sprite.height)
 					{
+						//award powerup
 						balls[d].lastHit->powerStatus[c] = powers[c].val;
 						powers[c].active = FALSE;
 						powers[c].available = FALSE;
@@ -633,6 +688,7 @@ void updatePowers() {
 				}
 			}
 		} else {
+			//try to spawn powerup
 			if (powers[c].available && rand() < powers[c].prb) {
 				powers[c].x = 10 + (float)(SCREEN_WIDTH-20)*(rand() / (float)RAND_MAX);
 				powers[c].y = (float)SCREEN_HEIGHT/4.0f + (float)SCREEN_HEIGHT / 2 * rand()/(float)RAND_MAX;
@@ -641,6 +697,7 @@ void updatePowers() {
 		}
 	}
 	
+	//spawn multiball if necessary
 	for (c = 0; c < 2; c++) {
 		if (players[c].powerStatus[MULTIBALL] != 0) {
 			if (players[c].powerStatus[MULTIBALL] % MULTI_DELAY == 0) {
@@ -668,40 +725,49 @@ void updatePowers() {
 	}
 }
 
+/* update the paddles */
 void updatePaddle() {
 	int c;
 	for (c = 0; c < 2; c++) {
 		if (players[c].ai) {
+			//calculate new position based on target position and time
 			if (players[c].paddle.collTime > 0)
 				players[c].paddle.x += clamp(
 							(players[c].paddle.collX - players[c].paddle.x)/(float)players[c].paddle.collTime--,
 							-MAX_PADDLE_SPEED, MAX_PADDLE_SPEED);
 		} else {
+			//use potentiometer to calculate position
 			players[c].paddle.x = (float)(SCREEN_WIDTH - paddleWidth(c)) * (float)(c == 0 ? readPotentiometer0() : readPotentiometer2()) / (float)POTEN_MAX;
 		}
 	}
 }
 
+/* update the screen */
 void updateScreen() {
 	char str[40];
 	int c;
 	
 	RIT128x96x4Clear();
 
+	//draw paddles
 	drawSprite(paddleSprite(0), players[0].paddle.x, players[0].paddle.y);
 	drawSprite(paddleSprite(1), players[1].paddle.x, players[1].paddle.y);
 
+	//draw powerups
 	for (c = 0; c < NUM_POWERS; c++) {
 		if (powers[c].active) {
 			drawSprite(&powers[c].sprite, powers[c].x, powers[c].y);
 		}
 	}
+	
+	//draw balls
 	for (c = 0; c < NUM_BALLS; c++) {
 		if (balls[c].active) {
 			drawSprite(&balls[c].sprite, balls[c].x, balls[c].y);
 		}
 	}
 	
+	//draw scores if no scoreboard
 	if (!scoreboard) {
 		sprintf(str, "%d", players[0].points);
 		RIT128x96x4StringDraw(str, 0, 40, 15);
@@ -710,71 +776,12 @@ void updateScreen() {
 	}
 }
 
+/* draw a sprite on the screen */
 void drawSprite(Sprite *s, float x, float y) {
-	int xPos = (int)(x - s->xOffset);
-	int yPos = (int)(y - s->yOffset);
-	float xf = (x - s->xOffset) - xPos;
-	float yf = (y - s->yOffset) - yPos;
-	bool even = xPos%2 == 0;
-	int xc, yc;
-	for (xc = 0; xc <= s->width/2; xc++) {
-		for (yc = 0; yc <= s->height; yc++) {
-			s->raster[yc*(s->width/2+1)+xc] = getColor(s, xc, yc, xf, yf, even);
-		}
-	}
-	RIT128x96x4ImageDraw(s->raster, xPos, yPos, s->width+2, s->height+1);
+	RIT128x96x4ImageDraw(s->data, (int)x - s->xOffset, (int)y - s->yOffset, s->width, s->height);
 }
 
-unsigned char getColor(Sprite *s, int xc, int yc, float xf, float yf, bool even) {
-	float color0 = 0;
-	float color1 = 0;
-	unsigned char pix;
-	if (even) {
-		if (xc-1 >= 0 && yc < s->height) {
-			pix = s->data[yc*s->width/2 + xc-1];
-			color1 += yf*(1-xf)*(pix&0x0F);
-		}
-		if (yc-1 >= 0 && xc < s->width/2) {
-			pix = s->data[(yc-1)*s->width/2 + xc];
-			color0 += (1-yf)*(1-xf)*(pix>>4);
-			color0 += (1-yf)*xf*(pix&0x0F);
-			color1 += (1-yf)*xf*(pix>>4);
-		}
-		if (yc-1 >= 0 && xc-1 >= 0) {
-			pix = s->data[(yc-1)*s->width/2 + xc-1];
-			color1 += (1-yf)*(1-xf)*(pix&0x0F);
-		}
-		if (yc < s->height && xc < s->width/2) {
-			pix = s->data[yc*s->width/2 + xc];
-			color0 += yf*(1-xf)*(pix>>4);
-			color0 += yf*xf*(pix&0x0F);
-			color1 += yf*xf*(pix>>4);
-		}
-	} else {
-		if (xc-1 >= 0 && yc < s->height) {
-			pix = s->data[yc*s->width/2 + xc-1];
-			color1 += yf*(1-xf)*(pix>>4);
-			color1 += yf*xf*(pix&0x0F);
-			color0 += yf*(1-xf)*(pix&0x0F);
-		}
-		if (yc-1 >= 0 && xc < s->width/2) {
-			pix = s->data[(yc-1)*s->width/2 + xc];
-			color0 += (1-yf)*xf*(pix>>4);
-		}
-		if (yc-1 >= 0 && xc-1 >= 0) {
-			pix = s->data[(yc-1)*s->width/2 + xc-1];
-			color1 += (1-yf)*(1-xf)*(pix>>4);
-			color1 += (1-yf)*xf*(pix&0x0F);
-			color0 += (1-yf)*(1-xf)*(pix&0x0F);
-		}
-		if (yc < s->height && xc < s->width/2) {
-			pix = s->data[yc*s->width/2 + xc];
-			color0 += yf*xf*(pix>>4);
-		}
-	}
-	return (((unsigned char)color1) << 4) | ((unsigned char)color0);
-}
-
+/* update collision information for ai players */
 void updateColl(int player) {
 	float pos;
 	int minTime = 1000000;
@@ -785,6 +792,7 @@ void updateColl(int player) {
 	if (!players[player].ai)
 		return;
 
+	//find the ball which will hit this side the soonest
 	for (c = 0; c < NUM_BALLS; c++) {
 		if (balls[c].active) {
 			ttime = (players[player].paddle.y - balls[c].y) / balls[c].dy;
@@ -795,8 +803,10 @@ void updateColl(int player) {
 		}
 	}
 	
+	//calculate the hit position
 	pos = (balls[minC].x + ((players[player].paddle.y - balls[minC].y) / balls[minC].dy) * balls[minC].dx);
 	pos -= FIELD_LEFT; //move pos to be aligned at left
+	//account for reflection off of walls
 	if (pos > 0) {
 		pos = fmod(pos, 2*FIELD_WIDTH);
 		if (pos >= FIELD_WIDTH)
@@ -808,14 +818,18 @@ void updateColl(int player) {
 			pos = 2*FIELD_WIDTH - pos;
 	}
 	pos += FIELD_LEFT; //move pos back into standard coords
+	//add a little randomness
 	pos += 6.0f*rand()/RAND_MAX - 3.0f;
+	//clamp onto the screen
 	players[player].paddle.collX = clamp(pos - paddleWidth(player)/2, 0, SCREEN_WIDTH - paddleWidth(player));
+	//update time to collision
 	players[player].paddle.collTime = minTime;
 }
 
+/* send updated data to scoreboard */
 void send() {
 	int i;
-	int powers = 0x0;
+	int powers;
 
 	if (!scoreboard)
 		return;
@@ -826,6 +840,8 @@ void send() {
 	game_stats[0] = players[0].points & 0x00FF;
 	game_stats[1] = players[1].points & 0x00FF;
 	
+	powers = 0;
+	//generate a bitmask from the powers
 	for(i = 0; i < NUM_POWERS; i++) {
 		if(players[0].powerStatus[i] != 0) {
 			powers = powers + (1 <<	i);
@@ -833,6 +849,7 @@ void send() {
 	}
 	game_stats[2] = powers;
 	
+	powers = 0;
 	for(i = 0; i < NUM_POWERS; i++) {
 		if(players[1].powerStatus[i] != 0) {
 			powers = powers + (1 <<	i);
@@ -847,12 +864,14 @@ void send() {
 	Xbee_SendArray(game_stats, SEND_ARRAY_LEN);
 }
 
-
+/* get the width of the given player's paddle */
 int paddleWidth(int player) {
 	return paddleSprite(player)->width;
 }
 
+/* get the sprite for the given player */
 Sprite *paddleSprite(int player) {
+	//use powerups to determine which paddle to use
 	if (players[player].powerStatus[LONG_PADDLE]) {
 		if (!players[1-player].powerStatus[SHORT_PADDLE]) {
 			return &players[player].paddle.longSprite;
@@ -863,16 +882,19 @@ Sprite *paddleSprite(int player) {
 	return &players[player].paddle.midSprite;
 }
 
+/* return the max of a and b */
 float max(float a, float b) {
 	return a>b ? a : b;
 }
+/* return the min of a and b */
 float min(float a, float b) {
 	return a<=b ? a : b;
 }
+/* clamp a into the range [cl, ch] */
 float clamp(float a, float cl, float ch) {
 	return max(cl, min(a, ch));
 }
-
+/* linearly interpolate between a and b with mix factor z */
 float mix(float a, float b, float z) {
 	return a*(1-z) + b*z;
 }
